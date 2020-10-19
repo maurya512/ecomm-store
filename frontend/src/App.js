1150cfd3e469dedbce8b8dd30182384be279a9fd
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-19 12:49:33 -0400

    added into app function from template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,90 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onclick="openMenu()">
+            &#9776;
+        </button>
+          <a href="index.html">Maurya's Store</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="login.html">Login</a>
+        </div>
       </header>
+      <aside className="sidebar">
+        <h3>Categories</h3>
+        <button className="sidebar-closeButton" onclick="closeMenu()">
+          X
+        </button>
+        <ul>
+          <li>
+            <a href="index.html">Laptops</a>
+          </li>
+          <li>
+            <a href="index.html">Cell Phones</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            <li className="product">
+              <img className="product-image" src='./images/p1.jpg' alt="product 1 image"></img>
+              <div className="product-name">
+                <a href="product.html">Macbook Pro 2020 Retina Display</a>
+              </div>
+              <div className="product-brand">Apple</div>
+              <div className='product-price'>$999</div>
+              <div className="product-rating">4.75 (10 reviews)</div>
+            </li>
+            <li className="product">
+              <img className="product-image" src='./images/p1.jpg' alt="product 1 image"></img>
+              <div className="product-name">
+                <a href="product.html">Macbook Pro 2020 Retina Display</a>
+              </div>
+              <div className="product-brand">Apple</div>
+              <div className='product-price'>$999</div>
+              <div className="product-rating">4.75 (10 reviews)</div>
+            </li>
+            <li className="product">
+              <img className="product-image" src='./images/p1.jpg' alt="product 1 image"></img>
+              <div className="product-name">
+                <a href="product.html">Macbook Pro 2020 Retina Display</a>
+              </div>
+              <div className="product-brand">Apple</div>
+              <div className='product-price'>$999</div>
+              <div className="product-rating">4.75 (10 reviews)</div>
+            </li>
+            <li className="product">
+              <img className="product-image" src='./images/p1.jpg' alt="product 1 image"></img>
+              <div className="product-name">
+                <a href="product.html">Macbook Pro 2020 Retina Display</a>
+              </div>
+              <div className="product-brand">Apple</div>
+              <div className='product-price'>$999</div>
+              <div className="product-rating">4.75 (10 reviews)</div>
+
+            </li>
+            <li className="product">
+              <img className="product-image" src='./images/p1.jpg' alt="product 1 image"></img>
+              <div className="product-name">
+                <a href="product.html">Macbook Pro 2020 Retina Display</a>
+              </div>
+              <div className="product-brand">Apple</div>
+              <div className='product-price'>$999</div>
+              <div className="product-rating">4.75 (10 reviews)</div>
+
+            </li>
+
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        All rights reserved.
+    </footer>
     </div>
   );
 }
