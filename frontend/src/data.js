398158f8f25be7828bc4b706ce72f47a76027ed6
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-19 13:44:30 -0400

    array of objects for the products

---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+export default {
+    // products is an array of objects
+    products: [
+        {
+        // each object is product data
+        name: 'Macbook Air Retina Display 2020',
+        category: 'Laptops',
+        image: './images/p1.jpg',
+        price: 999,
+        brand: 'Apple',
+        rating: 4.75,
+        numReviews: 10
+    },
+        {
+        name: 'Macbook Pro Retina Display 2020',
+        category: 'Laptops',
+        image: './images/p2.jpg',
+        price: 1299,
+        brand: 'Apple',
+        rating: 4.65,
+        numReviews: 12
+    },
+    {
+        name: 'iMac Pro Retina Display 2020',
+        category: 'Laptops',
+        image: './images/p3.jpg',
+        price: 1399,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 13
+    },
+]
+}
