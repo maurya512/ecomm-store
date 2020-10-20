77bce49258cb821ed6204795e0fef83a9db81aa0
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-19 22:48:28 -0400

    added links and elements to the products page which when clicked would display all info of prodcuts

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import data from '../data';
+import { Link } from 'react-router-dom';
 
 function ProductScreen(props) {
-    return <div>ProductScreen</div>
+    // we have access to the id of the products by doing as follows
+    console.log(props.match.params.id);
+    const product = data.products.find(x => x._id === props.match.params.id);
+    return <div>
+        <div>
+            {/* a link to go back to main page */}
+            <Link to='/'>Back to results</Link>
+        </div>
+        <div className='details'>
+            {/* displays the name of the details at the top of the page */}
+            <div className='details-image'>
+                <img src={product.image} alt="product"></img>
+            </div>
+            <div className='details-info'>
+                <ul>
+                    <li>
+                        <h4>{product.name} </h4>
+                    </li>
+                    <li>
+                        <h5>{product.rating} Stars ({product.numReviews})</h5>
+                    </li>
+                    <li>
+                        <b>{product.price}</b>
+                    </li>
+                    <li>
+                        Description:
+                        <div>
+                            {product.description}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 }
 
 export default ProductScreen;
