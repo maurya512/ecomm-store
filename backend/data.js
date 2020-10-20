58f5278ff90465449190c89caaa0637469f052a5
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-20 11:19:39 -0400

    product data file

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+export default {
+    // products is an array of objects
+    products: [
+        {
+        _id: '1',
+        // each object is product data
+        name: 'Macbook Air Retina Display 2020',
+        category: 'Laptops',
+        image: '../images/p1.jpg',
+        price: 999,
+        brand: 'Apple',
+        rating: 4.75,
+        numReviews: 10
+    },
+        {
+        _id: '2',
+        name: 'Macbook Pro Retina Display 2020',
+        category: 'Laptops',
+        image: '../images/p2.jpg',
+        price: 1299,
+        brand: 'Apple',
+        rating: 4.65,
+        numReviews: 12
+    },
+    {
+        _id: '3',
+        name: 'iMac Pro Retina Display 2020',
+        category: 'Laptops',
+        image: '../images/p3.jpg',
+        price: 1399,
+        brand: 'Apple',
+        rating: 4.5,
+        numReviews: 13
+    },
+]
+}
