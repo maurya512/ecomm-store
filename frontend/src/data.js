339cc0b33e5a348d118a31e7e535bc9dd4ca5c95
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-19 22:25:14 -0400

    editted the links to get images for the products

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -5,7 +5,7 @@ export default {
         // each object is product data
         name: 'Macbook Air Retina Display 2020',
         category: 'Laptops',
-        image: './images/p1.jpg',
+        image: '../images/p1.jpg',
         price: 999,
         brand: 'Apple',
         rating: 4.75,
@@ -14,7 +14,7 @@ export default {
         {
         name: 'Macbook Pro Retina Display 2020',
         category: 'Laptops',
-        image: './images/p2.jpg',
+        image: '../images/p2.jpg',
         price: 1299,
         brand: 'Apple',
         rating: 4.65,
@@ -23,7 +23,7 @@ export default {
     {
         name: 'iMac Pro Retina Display 2020',
         category: 'Laptops',
-        image: './images/p3.jpg',
+        image: '../images/p3.jpg',
         price: 1399,
         brand: 'Apple',
         rating: 4.5,
