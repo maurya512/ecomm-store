5f70dffc94f1cdd510f95571681b24d4ba8ac5ea
Maurya Patel <patelmaurya0512@gmail.com>
2020-10-19 22:57:54 -0400

    added the quantity and other elements for the products

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -35,6 +35,28 @@ function ProductScreen(props) {
                     </li>
                 </ul>
             </div>
+            <div className="details-action">
+                <ul>
+                    <li>    
+                        Price: {product.price}
+                    </li>
+                    <li>
+                        Status: {product.status}
+                    </li>
+                    <li>
+                        Qty: <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button> Add to Cart </button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 }
